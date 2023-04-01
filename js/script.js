a4457b4bb0aa304c0fd019c0251f463cc31f2d56
1xtend <vlad.mezhevyi@gmail.com>
2023-04-01 12:07:16 +0300

    hero section

--- js/script.js
+++ js/script.js
@@ -3,26 +3,29 @@ const navbar = document.querySelector('.navbar');
 const headerContent = document.querySelector('.header__content');
 const body = document.body;
 
-burger.addEventListener('click', (e) => {
+function active() {
   burger.classList.toggle('is-active');
   navbar.classList.toggle('is-active');
   body.classList.toggle('is-fixed');
   headerContent.classList.toggle('is-active');
+}
+
+function inactive() {
+  burger.classList.remove('is-active');
+  navbar.classList.remove('is-active');
+  body.classList.remove('is-fixed');
+  headerContent.classList.remove('is-active');
+}
+
+burger.addEventListener('click', (e) => {
+  active();
 
   headerContent.addEventListener('click', (e) => {
     if (e.target === e.currentTarget) {
-      burger.classList.remove('is-active');
-      navbar.classList.remove('is-active');
-      body.classList.remove('is-fixed');
-      headerContent.classList.remove('is-active');
+      inactive();
     }
   });
   navbar.querySelectorAll('.to-close').forEach((elem) => {
-    elem.addEventListener('click', (e) => {
-      burger.classList.remove('is-active');
-      navbar.classList.remove('is-active');
-      body.classList.remove('is-fixed');
-      headerContent.classList.remove('is-active');
-    });
+    elem.addEventListener('click', inactive);
   });
 });
